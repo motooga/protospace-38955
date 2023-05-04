8650c579cbf986254555d88d03b1ef9ec7c898a7
motooga <hashioga2017@gmail.com>
2023-05-05 07:30:40 +0900

    deviseの導入

---
+++ db/migrate/20230504214659_add_affliation_to_users.rb
@@ -0,0 +1,5 @@
+class AddAffliationToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :occupation, :text
+  end
+end
---
+++ db/migrate/20230504214706_add_director_to_users.rb
@@ -0,0 +1,5 @@
+class AddDirectorToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :position, :text
+  end
+end
